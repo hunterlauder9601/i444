ba8da6e7e4aef663f7416f6fc1fff1fcc36cb112
Hunter Lauder <hlauder1@CS544-hlauder1.bu.binghamton.edu>
2022-11-25 05:25:55 +0000

    update 1

--- prj4-sol/src/js/search-widget.mjs
+++ prj4-sol/src/js/search-widget.mjs
@@ -12,11 +12,34 @@ import { doFetchJson } from './util.mjs';
 class SearchWidget extends HTMLElement {
   constructor() {
     super();
-    //TODO
+    const shadow = this.shadow = this.attachShadow({mode: "closed"});
+    let template = document.querySelector('#search-widget');
+    shadow.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
-    //TODO
+    this.wsUrl = this.getAttribute('ws-url');
+    if(this.wsUrl === null || this.wsUrl === "") {
+      console.log("error: missing ws-url attribute");
+    }
+    this.queryParam = this.getAttribute('query-param');
+    if(this.queryParam === null || this.queryParam === "") {
+      console.log("error: missing queryParam attribute");
+    }
+    this.resultWidget = this.getAttribute('result-widget');
+    if(this.resultWidget === null || this.resultWidget === "") {
+      console.log("error: missing resultWidget attribute");
+    }
+    this.label = this.getAttribute('label');
+    if(this.resultWidget !== null && this.resultWidget !== "") {
+      const labelElem = this.shadow.querySelector('slot[name="label"]');
+      labelElem.textContent = this.label;
+    }
+
+    this.shadow.querySelector('input#search').addEventListener('input', (ev) => {
+      console.log(ev.target.value);
+    })
+
   }
 
   //TODO: add private methods  
